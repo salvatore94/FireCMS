9c3255005a6ec9772aa8ac01ab0b10ed00d4adb5
Salvatore Polito <thedarkhacker94@gmail.com>
2017-03-25 00:38:09 +0100

    Firebase signup Registerviewcontroller

---
+++ FireCMS/SignupViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SignupViewController.swift
+//  FireCMS
+//
+//  Created by Salvatore  Polito on 24/03/17.
+//  Copyright © 2017 Salvatore  Polito. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignupViewController: UIViewController {
+
+
+    @IBOutlet weak var emailTextField: UITextField!
+
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func register(_ sender: Any) {
+        guard let email = emailTextField.text else {
+            return
+        }
+        guard let password = passwordTextField.text else {
+            return
+        }
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+            guard error == nil else {
+                let alertVC = UIAlertController(title: "Errore", message: "Riempire i campi di testo!", preferredStyle: UIAlertControllerStyle.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in
+                    print("ok")
+                })
+                alertVC.addAction(okButton)
+                self.present(alertVC, animated: true, completion: nil)
+                return
+            }
+            
+            print("\n Welcome \(user!.email!)")
+        })
+        
+        
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
